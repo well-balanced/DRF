6e5bc3f82b39be80f954dcf48a863fe7d5050c9e
well-balanced <softkey95@gamil.com>
2020-02-27 21:59:05 +0900

    Add permission ReadOnly to Strangers

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,17 +1,18 @@
 # from django.http import HttpResponse, JsonResponse
 # from django.views.decorators.csrf import csrf_exempt
 # from rest_framework.parsers import JSONParser
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 
 from django.http import Http404
 from rest_framework.views import APIView
 
 from rest_framework import mixins
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 # @csrf_exempt
 # def snippet_list(request):
@@ -160,7 +161,20 @@ from rest_framework import generics
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
